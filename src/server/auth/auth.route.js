0fd398761e124935df02ca86ed9ae626ab57a735
Kuan-Ying Fang <kuanyingfang@gmail.com>
2018-12-07 21:18:19 -0800

    adding auth route

--- src/server/auth/auth.route.js
+++ src/server/auth/auth.route.js
@@ -4,4 +4,23 @@ const router = express.Router(); // eslint-disable-line new-cap
 
 // #TODO: Implement authentication routes.
 
+router.route('/sign-up')
+.all()
+.get()
+.post();
+
+router.route('/login')
+.all()
+.get()
+.post()
+.put();
+
+router.get('/logout');
+
+router.delete('/');
+
+// router.post('/sign-up', );
+// router.post('/login', );
+// router.put('/login', );
+
 module.exports = router;
