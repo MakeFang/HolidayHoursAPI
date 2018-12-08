42b6426052f83230f0bbe05083cf7d88708ec4c2
Kuan-Ying Fang <kuanyingfang@gmail.com>
2018-12-07 21:20:44 -0800

    write stub test for auth

--- src/server/auth/auth.test.js
+++ src/server/auth/auth.test.js
@@ -1,7 +1,31 @@
 const chai = require('chai'); // eslint-disable-line import/newline-after-import
+const expect = chai.expect;
 
 chai.config.includeStack = true;
+// const chaiHttp = require('chai-http');
+// chai.use(chaiHttp);
 
 describe('## Auth APIs', () => {
-  // # TODO: Implement Authentication Tests.
+    // # TODO: Implement Authentication Tests.
+  it('Should test 3!=2', (done) => {
+    expect(3).to.be.not.equal(2);
+    done();
+  });
+
+});
+
+describe('Testing Authenticated Routes', () => {
+
+});
+
+describe('Testing Authorized Routes', () => {
+
+});
+
+describe('Testing Unauthenticated Routes', () => {
+
+});
+
+describe('Testing Unauthorized Routes', () => {
+
 });
